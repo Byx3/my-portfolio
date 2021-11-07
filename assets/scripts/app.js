449665abbe3264977eb54a88be8c00d4e5f59ab0
Byx3 <porcaro@me.com>
2021-11-07 16:40:08 +0100

    customized wrapper & name

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,15 +57,15 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a vision-driven change agent.", "solve problems.", "stand for curiosity, integrity, entrepreneurship."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
-			typeSpeed: 1,
+			typeSpeed: 10,
 			contentType: 'text',
 			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+				$("#writing-text").css({"color": "#fff", "background-color": "#A17A9F"});
 			},
 			preStringTyped: function() {},
 			onStringTyped: function() {}
